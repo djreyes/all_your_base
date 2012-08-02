562ae55651e3a915eb1cf0cccb93d058d03f62ca
Patrick Cushing + Jeremy Reyes <apprentice+pc+jr@devbootcamp.com>
2012-08-01 17:12:50 -0700

    navbar sign in, out, and status; removed sidebar by default and added only for projects page

--- spec/requests/shared_pages_spec.rb
+++ spec/requests/shared_pages_spec.rb
@@ -2,13 +2,36 @@ require 'spec_helper'
 
 describe "Shared pages" do
   subject { page }
+  before { visit root_path }
+  let(:user) { Fabricate(:user) }
 
-  describe "home page" do
-    before { visit root_path }
+  it { should have_selector('a', text: 'AllYourBase') }
 
-    it { should have_selector('a', text: 'AllYourBase') }
-    it { should have_selector('a', text: 'Sign up') }
-    it { should have_selector('a', text: 'Sign in') }
-    it { should have_selector('a', text: 'Sign out') }
+  describe "navbar" do
+    describe "when logged out" do
+      it { should have_selector('a', text: 'Sign up') }
+      it { should have_selector('a', text: 'Sign in') }
+      it { should_not have_selector('a', text: 'Sign out') }
+    end
+
+    describe "when logged in" do
+      before do
+        visit new_user_session_path
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should_not have_selector('a', text: 'Sign up') }
+      it { should_not have_selector('a', text: 'Sign in') }
+      it { should have_selector('a', text: 'Sign out') }
+      it { should have_content(user.email) }
+
+      describe "clicking on user email takes you to projects" do
+        before { click_link user.email }
+
+        it { should have_content('Projects') }
+      end
+    end
   end
 end
